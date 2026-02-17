92a1ae955d42b36b10af77fe0701cfdd1de99b5a
Thanawich Juengkijthanawat <thanawich.jue@student.mahidol.edu>
2026-02-18 01:18:33 +0700

    change area min

--- BottleCounter.py
+++ BottleCounter.py
@@ -6,14 +6,12 @@ def count_bottles(image_path):
 
     # Perspective Transform เพื่อแก้ไขมุมมองของภาพจากเอียงเป็นมุมตรง
     rows,cols,ch = img.shape
-    pts1 = np.float32([[228,4],[1175,12],[112,605],[1285,613]])
+    pts1 = np.float32([[234,7],[1170,12],[120,610],[1280,605]])
     pts2 = np.float32([[0,0],[800,0],[0,800],[800,800]])
 
     M = cv2.getPerspectiveTransform(pts1,pts2)
     img = cv2.warpPerspective(img,M,(800,800))
 
-    # height, width, _ = img.shape
-    # img = img[5:height-30, 130:width-10]
 
     # Convert image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -26,7 +24,7 @@ def count_bottles(image_path):
     threshold = cv2.adaptiveThreshold(grayBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 1)
 
 
-    ### ลองใช้ Morphology operations
+    ### Morphology operations
     kernel = np.ones((2,2), np.uint8)
     opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=3)
 
@@ -42,18 +40,22 @@ def count_bottles(image_path):
     kernel = np.ones((3,3), np.uint8)
     closing = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel, iterations=3)
 
+    kernel = np.ones((4,4), np.uint8)
+    dilation = cv2.dilate(closing, kernel, iterations=5)
+
     kernel = np.ones((5,5), np.uint8)
-    dilation = cv2.dilate(closing, kernel, iterations=3)
-    ### ลองใช้ Morphology operations
+    erosion = cv2.erode(dilation, kernel, iterations=2)
+
+    ## Morphology operations
 
 
-    result_img = dilation.copy()
+    result_img = erosion.copy()
     contours, hierarchy = cv2.findContours(result_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     # วาดวงกลมรอบๆ ขวด
     counter = 0
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area>500 and area < 5000:
+        if area>200 and area < 1200:
             if len(cnt) >= 5:
                 ellipse = cv2.fitEllipse(cnt)
                 cv2.ellipse(img, ellipse, (0,255,0), 2)
@@ -61,6 +63,7 @@ def count_bottles(image_path):
 
 
     cv2.putText(img, str(counter), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 4, (0,0,255), 2, cv2.LINE_AA)
+    cv2.imshow('Edges', erosion)
     cv2.imshow('Bottles', img)
     print(f'Number of bottles detected: {counter}')
     k = cv2.waitKey(0)
